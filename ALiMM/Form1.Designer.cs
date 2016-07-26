0876ce4cb17409bbf4a71fcb5e1f5ac7f9110da7
jhkmnm <jhkmnm_123@163.com>
2016-07-26 20:36:49 +0800

    提交

---
+++ ALiMM/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ALiMM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblJson = new System.Windows.Forms.Label();
+            this.tmRef = new System.Windows.Forms.Timer(this.components);
+            this.tmCheckLoad = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtAccount
+            // 
+            this.txtAccount.Location = new System.Drawing.Point(52, 21);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.Size = new System.Drawing.Size(172, 21);
+            this.txtAccount.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "账号:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密码:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(301, 21);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(174, 21);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(497, 19);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(699, 21);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(62, 21);
+            this.txtTime.TabIndex = 6;
+            this.txtTime.Text = "2";
+            this.txtTime.Visible = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(6, 64);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1347, 670);
+            this.webBrowser1.TabIndex = 8;
+            this.webBrowser1.Url = new System.Uri("http://url.cn/2Ie8OSI", System.UriKind.Absolute);
+            this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblJson);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtAccount);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtTime);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnLogin);
+            this.groupBox1.Location = new System.Drawing.Point(6, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1347, 57);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(648, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 9;
+            // 
+            // lblJson
+            // 
+            this.lblJson.AutoSize = true;
+            this.lblJson.Location = new System.Drawing.Point(898, 24);
+            this.lblJson.Name = "lblJson";
+            this.lblJson.Size = new System.Drawing.Size(0, 12);
+            this.lblJson.TabIndex = 8;
+            // 
+            // tmRef
+            // 
+            this.tmRef.Tick += new System.EventHandler(this.tmRef_Tick);
+            // 
+            // tmCheckLoad
+            // 
+            this.tmCheckLoad.Tick += new System.EventHandler(this.tmCheckLoad_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 781);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer tmRef;
+        private System.Windows.Forms.Timer tmCheckLoad;
+        private System.Windows.Forms.Label lblJson;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ALiMM/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+
+namespace ALiMM
+{
+    public partial class Form1 : Form
+    {
+        string text, title, url;
+        int checkdocumenttime = 0;
+        bool islogin = false;
+        bool canlogin = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void Login()
+        {
+            try
+            {
+                var doc = this.webBrowser1.Document;
+                var user = doc.GetElementById("J_QuickLogin").GetElementsByTagName("input").GetElementsByName("user");
+
+                if (user.Count > 0)
+                {
+                    var J_SubmitQuick = doc.GetElementById("J_SubmitQuick");
+                    J_SubmitQuick.InvokeMember("click");
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(txtAccount.Text) || string.IsNullOrEmpty(txtPassword.Text))
+                    {
+                        MessageBox.Show("请输入账号和密码");
+                        return;
+                    }
+
+                    var username = doc.GetElementById("TPL_username_1");
+                    var password = doc.GetElementById("TPL_password_1");
+                    var login = doc.GetElementById("J_SubmitStatic");
+                    username.SetAttribute("value", txtAccount.Text);
+                    password.SetAttribute("value", txtPassword.Text);
+                    login.InvokeMember("click");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("请检查是否已经登录，如果页面没有加载出来，请关掉重试");
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SetInterval();
+            Login();
+        }
+
+        private void SetInterval()
+        {
+            int time = 0;
+            time = int.TryParse(txtTime.Text, out time) ? time : 60;
+            tmRef.Interval = time * 1000 * 60;
+        }
+
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            Check();
+        }
+
+        private void Check()
+        {
+            tmCheckLoad.Enabled = true;
+            text = title = url = "";
+            checkdocumenttime = 0;
+        }
+
+        private void tmRef_Tick(object sender, EventArgs e)
+        {
+            tmRef.Enabled = false;            
+            if(!CheckLogin())
+            {
+                webBrowser1.Refresh();
+                Check();
+            }
+            label3.Text = "刷新"+DateTime.Now;
+            tmRef.Enabled = true;
+        }
+
+        private bool CheckLogin()
+        {
+            try
+            {
+                string str = Get_Method();
+                var v = JsonConvert.DeserializeObject<UnionInfo>(str);
+                string log = string.Format("{1}!{0} 返回结果:{2}", Environment.NewLine, v.data.noLogin ? "Cookies失效" : "Cookies有效", str);
+                WriteFile(log);
+                return !v.data.noLogin;
+                //var div = webBrowser1.Document.GetElementById("J_menu_product").Children[0];
+                //return div.OuterText.Contains("，");
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 检查页面是否加载完成
+        /// </summary>
+        private void CheckDocumentLoad()
+        {
+            if (webBrowser1.ReadyState == WebBrowserReadyState.Complete && !webBrowser1.IsBusy)
+            {
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    text = webBrowser1.DocumentText;
+                    title = webBrowser1.DocumentTitle;
+                    url = webBrowser1.Url.ToString();
+                }
+                else
+                {
+                    if (webBrowser1.DocumentText == text && title == webBrowser1.DocumentTitle &&
+                        url == webBrowser1.Url.ToString())
+                    {
+                        checkdocumenttime += 1;
+                    }
+                    else
+                    {
+                        text = webBrowser1.DocumentText;
+                        title = webBrowser1.DocumentTitle;
+                        url = webBrowser1.Url.ToString();
+                        checkdocumenttime = 0;
+                    }
+                }
+            }
+        }
+
+        private void tmCheckLoad_Tick(object sender, EventArgs e)
+        {
+            tmCheckLoad.Enabled = false;
+            try
+            {
+                if (checkdocumenttime == 5)
+                {
+                    // = CheckLogin();
+                    if (CheckLogin())
+                    {
+                        if (!islogin)
+                        {
+                            lblJson.Text = POST_Method(webBrowser1.Document.Cookie);
+                            tmRef.Enabled = true;
+                            canlogin = false;
+                            islogin = true;                            
+                        }
+                    }
+                    else
+                    {
+                        islogin = false;
+                        if (canlogin)
+                        {
+                            Login();
+                        }
+                        else
+                        {
+                            webBrowser1.Url = new Uri("http://url.cn/2Ie8OSI");
+                            canlogin = true;
+                        }
+                    }
+                }
+                else
+                {
+                    CheckDocumentLoad();
+                    tmCheckLoad.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                tmCheckLoad.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// 发送cookies
+        /// </summary>
+        /// <param name="postdata"></param>
+        /// <returns></returns>
+        private string POST_Method(string postdata)
+        {
+            try
+            {
+                postdata = GetCookies("http://www.alimama.com/index.htm");
+                WriteFile(string.Format("登录成功!{0}Cookies:{1}", Environment.NewLine, postdata));
+                var data = "type=2&cookie=" + postdata;                
+                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+                byte[] arrB = encode.GetBytes(data);
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("http://api.dev.doukou.com/data/setcookie");
+                myReq.Method = "POST";
+                myReq.ContentType = "application/x-www-form-urlencoded";
+                myReq.ContentLength = arrB.Length;
+                Stream outStream = myReq.GetRequestStream();
+                outStream.Write(arrB, 0, arrB.Length);
+                outStream.Close();
+
+                WebResponse myResp = myReq.GetResponse();
+                Stream ReceiveStream = myResp.GetResponseStream();
+                StreamReader readStream = new StreamReader(ReceiveStream, encode);
+                Char[] read = new Char[256];
+                int count = readStream.Read(read, 0, 256);
+                string str = null;
+                while (count > 0)
+                {
+                    str += new String(read, 0, count);
+                    count = readStream.Read(read, 0, 256);
+                }
+                readStream.Close();
+                myResp.Close();
+                return str;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private string Get_Method()
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create("http://pub.alimama.com/common/getUnionPubContextInfo.json") as HttpWebRequest;
+                request.Headers.Add(HttpRequestHeader.Cookie, GetCookies("http://www.alimama.com/index.htm"));
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    Stream stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream, Encoding.GetEncoding("UTF-8"));
+                    string html = reader.ReadToEnd();
+                    reader.Close();
+                    return html;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool InternetGetCookieEx(string pchURL, string pchCookieName, StringBuilder pchCookieData, ref System.UInt32 pcchCookieData, int dwFlags, IntPtr lpReserved);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetInterval();
+        }
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int InternetSetCookieEx(string lpszURL, string lpszCookieName, string lpszCookieData, int dwFlags, IntPtr dwReserved);
+        
+        private static string GetCookies(string url)
+        {
+            uint datasize = 256;
+            StringBuilder cookieData = new StringBuilder((int)datasize);
+            if (!InternetGetCookieEx(url, null, cookieData, ref datasize, 0x2000, IntPtr.Zero))
+            {
+                if (datasize < 0)
+                    return null;
+
+                cookieData = new StringBuilder((int)datasize);
+                if (!InternetGetCookieEx(url, null, cookieData, ref datasize, 0x00002000, IntPtr.Zero))
+                    return null;
+            }
+            return cookieData.ToString();
+        }
+
+        public static void WriteFile(string txt)
+        {
+            string datatxt = DateTime.Now.ToShortDateString();
+            StreamWriter sw;
+            if (File.Exists("LOG.txt"))
+            {
+                sw = File.AppendText("LOG.txt");
+            }
+            else
+            {
+                sw = File.CreateText("LOG.txt");
+            }
+            sw.WriteLine(DateTime.Now + ":     " + txt);
+            sw.Close();
+            sw.Dispose();
+        }
+    }
+
+    public class LoginData
+    {
+        public bool noLogin { get; set; }
+    }
+
+    public class UnionInfo
+    {
+        public LoginData data { get; set; }
+    }
+}
